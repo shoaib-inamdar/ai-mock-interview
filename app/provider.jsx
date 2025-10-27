97741887b99916377f33f6595e0cd89b0cf79de1
shoaib inamdar <shoaibinamdar14@gmail.com>
2025-10-28 00:38:15 +0530

    feat: done with the convex db and docker setup locally

--- app/provider.jsx
+++ app/provider.jsx
@@ -1,16 +1,19 @@
 "use client";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
-import React from "react";
+import React, { Suspense } from "react";
+import AuthProvider from "./AuthProvider";
 
 function Provider({ children }) {
   const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
   return (
-    <div>
+    <Suspense fallback={<div>Loading...</div>}>
       <ConvexProvider client={convex}>
+        <AuthProvider>
           {children}
+        </AuthProvider>
       </ConvexProvider>;
-    </div>
+    </Suspense>
   )
 }
 
--- stack/server.js
+++ stack/server.js
@@ -1,6 +1,8 @@
 import "server-only";
 import { StackServerApp } from "@stackframe/stack";
+import { redirect } from "next/dist/server/api-utils";
 
 export const stackServerApp = new StackServerApp({
-  tokenStore: "nextjs-cookie", // storing auth tokens in cookies
+  tokenStore: "nextjs-cookie",
 });
+
