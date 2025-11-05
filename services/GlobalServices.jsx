0f485372248781988397f74d768bfc1de4212631
Priyanka Kumbhar <priyankakumbhar.2405@gmail.com>
2025-11-06 00:48:02 +0530

    Setting up an AI backend using Gemini

--- services/GlobalServices.jsx
+++ services/GlobalServices.jsx
@@ -1,6 +1,26 @@
 import axios from "axios"
-
+import OpenAI from "openai";
 export const getToken=async()=>{
     const result=await axios.get('/api/getToken');
     return result.data;
 }
+const openai = new OpenAI({
+  baseURL: "https://openrouter.ai/api/v1",
+  apiKey: process.env.NEXT_PUBLIC_AI_OPENROUTER,
+  dangerouslyAllowBrowser:true
+});
+
+export const AIModel = async (topic, coachingOption, msg) => {
+  const option = coachingOption.find((item) => item.name == coachingOption);
+  const PROMPT = option.prompt.replace("{user_topic}", topic);
+
+  const completion = await openai.chat.completions.create({
+    model: "google/gemini-2.0-flash-exp:free",
+    messages: [
+      { role: "assistant", content: PROMPT },
+      { role: "user", content: msg },
+    ],
+  });
+
+  console.log(completion.choices[0].message);
+};
