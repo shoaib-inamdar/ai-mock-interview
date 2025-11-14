50de572dccedfe52a239a10f8d8f3ce103c76cce
Priyanka Kumbhar <priyankakumbhar.2405@gmail.com>
2025-11-14 22:21:26 +0530

    added webcam and user profile

--- app/(main)/dashboard/_components/Credits.jsx
+++ app/(main)/dashboard/_components/Credits.jsx
@@ -2,12 +2,21 @@ import React, { useContext } from 'react'
 import { UserContext } from '@/app/_context/UserContext';
 import Image from 'next/image';
 import { useUser } from '@stackframe/stack';
+import { Progress } from '@/components/ui/progress';
+import { Button } from '@/components/ui/button';
+import { Wallet2 } from 'lucide-react';
 
 
 function Credits() {
 
     const {userData} = useContext(UserContext);
     const user = useUser();
+    const CalculateProgress=()=>{
+      if(userData?.subscriptionId){
+        return  Number(userData.Credits/50000)*100
+      }
+
+    }
   return (
     <div>
         <div className='flex gap-5 items-center'>
@@ -20,7 +29,30 @@ function Credits() {
             </div>
         </div>
         <hr className='my-3'></hr>
-        
+        <div>
+          <h2 className='font-bold'>Token Usage</h2>
+          <h2>{userData.credits}/{userData?.subscriptionId ? '50,000':'5000'}</h2>
+          <Progress value={CalculateProgress()} className='my-3'/>
+
+          <div className='flex justify-between items-center mt-3' >
+            <h2 className='font-bold'> Current plan  </h2>
+            <h2 className='p-1 bg-secondary rounded-lg px-2'>
+
+{userData?.subscriptionId ? 'Paid Plan':'Free Plan'}
+</h2>
+          </div>
+          <div className='mt-5 p-5 border rounded-2xl'>
+          <div className='flex justify-between'>
+            <div>
+              <h2>Pro Plan </h2>
+              <h2>50,000 Tokens</h2>
+            </div>
+            <h2 className='font-bold'>$10/Month</h2>
+          </div>
+          <hr className='my-3'/>
+          <Button className='w-full'> <Wallet2/>  Upgrade  $10</Button>
+        </div>
+</div>
     </div>
   )
 }
